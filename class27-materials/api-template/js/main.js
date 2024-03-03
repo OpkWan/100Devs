d9461a45a8a9a66674e8077b16904377ee8b735a
OpkWan <alexopoku83@gmail.com>
2024-03-03 21:51:00 +0000

    Learned about APIs

--- class27-materials/api-template/js/main.js
+++ class27-materials/api-template/js/main.js
@@ -2,13 +2,17 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  // const choice = document.querySelector('input').value
+  const url = 'https://lucifer-quotes.vercel.app/api/quotes'
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        document.querySelector('h2').innerText = data[0].quote
+        document.querySelector('h3').innerText = data[0].author
+        // document.querySelector('img').src = data.types[0].type.url
+        document.querySelector('h3').innerText = data[0].author
       })
       .catch(err => {
           console.log(`error ${err}`)
