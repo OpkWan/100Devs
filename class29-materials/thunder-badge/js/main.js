d52f9877907ee7f2a7ca28b7cc9f370c83317c1d
OpkWan <alexopoku83@gmail.com>
2024-03-10 21:16:20 +0000

    Created a new class under thunder badge

--- class29-materials/thunder-badge/js/main.js
+++ class29-materials/thunder-badge/js/main.js
@@ -1,2 +1,20 @@
 //Objects
 //Please create a pokemon class with as much detail as possible in 10 minutes
+
+class Dome {
+  constructor(name, age, sex) {
+    this.name = name;
+    this.age = age;
+    this.sex = sex;
+  }
+
+  desc() {
+    console.log(
+      `My name is ${this.name}, I am ${this.age} years old and I am a ${this.sex}`
+    );
+  }
+}
+
+let opk = new Dome("Alex", 30, "male");
+
+console.log(opk);
