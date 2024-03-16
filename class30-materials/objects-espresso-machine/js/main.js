6aedb5cf92b4ddd0e8910aea4db7f33eda34bb79
OpkWan <alexopoku83@gmail.com>
2024-03-16 23:13:47 +0000

    I learnt about Inheritance and Polymorphism in OOP

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -12,25 +12,78 @@
 //   g() {}
 // }
 
-class EspressoMachine {
-  constructor(color, make, model, price) {
-    this.color = color;
-    this.make = make;
-    this.model = model;
-    this.price = price;
-  }
+// class EspressoMachine {
+//   constructor(color, make, model, price) {
+//     this.color = color;
+//     this.make = make;
+//     this.model = model;
+//     this.price = price;
+//   }
 
-  turnOn() {
-    console.log(`Good day, I am now on!`);
-  }
+//   turnOn() {
+//     console.log(`Good day, I am now on!`);
+//   }
 
-  steam() {
-    console.log(`ITTSSS SUMMER SALLEEE 50%%%`);
-  }
+//   steam() {
+//     console.log(`ITTSSS SUMMER SALLEEE 50%%%`);
+//   }
 
-  brew() {
-    console.log(`Good stuff coming your way`);
+//   brew() {
+//     console.log(`Good stuff coming your way`);
+//   }
+// }
+
+// let gaggia = new EspressoMachine("red", "Gaggia", "Classic Pro", 400);
+
+// function convertToCelsius(temperature) {
+//   let celsius;
+
+//   return celsius;
+// }
+
+// function weatherInfo(temperature) {
+//   let c = (temperature - 32) * (5 / 9);
+//   console.log(c);
+//   if (c < 0) {
+//     return c + " is freezing temperature";
+//   } else {
+//     return c + " is above freezing temperature";
+//   }
+// }
+
+// // console.log(convertToCelsius(50));
+// console.log(weatherInfo(23));
+
+// function howManyDalmations(number) {
+//   let dogs = [
+//     "Hardly any",
+//     "More than a handful!",
+//     "Woah that's a lot of dogs!",
+//     "101 DALMATIONS!!!",
+//   ];
+
+//   let respond =
+//     number <= 10
+//       ? dogs[0]
+//       : number <= 50
+//       ? dogs[1]
+//       : number >= 101
+//       ? dogs[3]
+//       : dogs[2];
+
+//   return respond;
+// }
+
+// console.log(howManyDalmations(101));
+
+function myFirstKata(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return false;
+  } else {
+    return `${a} % ${b} + ${b} % ${a}`;
   }
 }
 
-let gaggia = new EspressoMachine("red", "Gaggia", "Classic Pro", 400);
+let i = myFirstKata(67, "bye");
+
+console.log(i);
--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,27 @@
-//Create an a class and extend it - Can be anything you would like it to be! 
+//Create an a class and extend it - Can be anything you would like it to be!
 
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+  speak() {
+    console.log(`${this.name} makes a sound`);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, breed) {
+    super(name);
+    this.breed = breed;
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, gender) {
+    super(name);
+    this.gender = gender;
+  }
+}
+
+let simba = new Dog("Simba", "Sheperd");
+let meow = new Cat("Meow", "male");
